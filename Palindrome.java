8928348db37cf4f7b4fd3ae68bf726ab72108cec
fredericbaucher <>
2021-04-03 07:18:11 +0000

    QUESTION : one letter is missing when I run is_palindrome function

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,28 @@ public class Palindrome{
     public static boolean is_palindrome(String s)
     {
         // add your code here
-        return true;
+
+
+      String rvr = ""; // Objects of String class  
+      int length = s.length();   
+      for ( int i = length - 1; i >= 1; i-- )  
+          // I have a question : one letter is missing when I run is_palindrome function
+          /*
+          //  when I run test
+           java Principal kelek 
+          // I get this result
+          rvr : kele
+          ----------------------------------------------------
+          Input : kelek >>> Output : false
+          ----------------------------------------------------
+          */
+
+         rvr = rvr + s.charAt(i);  
+         System.out.println ( "rvr : " + rvr ) ;
+      if (s.equals(rvr))  
+         return true;  
+      else  
+         return false;
+      
     }
 }
